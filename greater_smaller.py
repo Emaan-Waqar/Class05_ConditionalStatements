99d9625eab193cd0b7e6f4fb7b715b7cafb56d39
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-07-15 13:12:30 +0500

    Greater or Smaller

---
+++ greater_smaller.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number: "))
+if (i<15):
+    print("i is smaller than 15")
+    print("I is in the If block")
+else:
+    print("i is greater than 15")
+    print("i is in the Else block")
+print("i is neither in If nor in Else block")        
