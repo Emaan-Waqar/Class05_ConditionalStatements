9490312b933c4022ce35da2b84ed5791d1db269f
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-07-15 13:01:02 +0500

    pr

---
+++ profitLoss.py
@@ -0,0 +1,8 @@
+cost_price= float(input("Enter the Actual price of the product: "))
+selling_price= float(input("Enter the selling price of the product: "))
+
+if selling_price >cost_price:
+    amount= selling_price- cost_price
+    print("Total profit= {0}".format(amount))
+else:
+    print("No profit!")
