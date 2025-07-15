e827d14452e08fc511ad3422371102a1d5f85028
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-07-15 13:21:22 +0500

    Weather

---
+++ assignment.py
@@ -0,0 +1,7 @@
+weather= input("Enter the season: ")
+weather= weather.lower()
+
+if weather== "winter":
+    print("The weather is cold outside!")
+else: 
+    print("No need to wear jacket and pullovers! 😊") 
